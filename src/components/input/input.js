0a61fa2126bf0ff3279a4afe91d28f1c5f5d1b8f
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-09-25 15:55:27 -0300

    Criando funções do cadastro

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 //onChange, value
-const Input = ({ name, type, placeholder, className }) => {
+const Input = ({ name, type, placeholder, className, value }) => {
   return (
     <input
       name={name}
       type={type}
       placeholder={placeholder}
       className={className}
+      value={value.name}
     />
   )
 }
