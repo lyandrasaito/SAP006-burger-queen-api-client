de769fab792f15d364d89fd8cebc232c6dc587f5
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-10-08 18:57:25 -0300

    Fetch da api (post order)

---
+++ src/services/postAPI.js
@@ -0,0 +1,13 @@
+export const postOrder = async (values) => {
+  const token = localStorage.getItem('token');
+  return await fetch('https://lab-api-bq.herokuapp.com/orders', {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      "Content-Type": "application/json",
+      'Authorization': `${token}`,
+    },
+    body: JSON.stringify(values)
+  }).then(res => res.json())
+  .catch(e => console.log(e))
+};
